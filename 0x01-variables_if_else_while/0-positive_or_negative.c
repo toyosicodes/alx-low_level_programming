bf0af5508ef9d702dcfe88cb7a0b7b4f1ece8080
Toyosi Salami <omotoyosisalami00@gmail.com>
2022-03-12 01:16:13 +0100

    +ve or -ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,13 +9,13 @@ int main(void)
     srand(time(0));
     n = rand() - RAND_MAX / 2;
     if (n < 0)
-        printf(n, " is negative");
+        printf(n, " is negative\n");
     else
         if (n = 0)
-            printf(n, " is zero");
+            printf(n, " is zero\n");
     else
-    if (n > 0)
-        printf(n, " is positive");
+        if (n > 0)
+            printf(n, " is positive\n");
     
     return (0);
 }
